a155174d56c9d7cf3d6672e322beba5018d3a8de
Jhonah <buryakone@gmail.com>
2020-10-07 15:25:41 +0700

    в section id=store по dataset(data-filter) из html смог найти и получить их значения

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-const body = document.querySelector('body');
+//const body = document.querySelector('body');
 const store = document.querySelector('#store');
 store.addEventListener('click', (event) => {
     event.preventDefault();
 })
+//const filterBtn = document.querySelector('#filter-btn');
+//console.log(filterBtn.dataset.filter);
 
-const buttons = document.querySelectorAll('filter-btn');
+const buttons = document.querySelectorAll('.filter-btn');
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        
+    button.addEventListener('click', () => {
+        if (button.dataset.filter === "all")
+          console.log(button.dataset.filter);
+        //if (button.classList.contains('filter-btn').dataset.filter.contains('all')) {
+        //}
     })
 })
